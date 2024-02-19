b978a990031b9d0cd5d505f74b46f3afcf3afc50
ED9IH <demanin@bk.ru>
2024-02-19 15:45:16 +0300

    123

---
+++ src/main/java/ru/demanin/spring/FirstRestApp/dto/SensorDTO.java
@@ -0,0 +1,21 @@
+package ru.demanin.spring.FirstRestApp.dto;
+
+import javax.persistence.Column;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+public class SensorDTO {
+
+    @NotEmpty(message = "Поле не должно быть пустым")
+    @Size(min=3,max = 30,message = "В название сенсора должно быть не меньше 3 и не более 30 символов")
+    @Column(name = "name")
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
